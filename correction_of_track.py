changed all math to mpmath library for infinite precision, corrected angle calculation from dot product to cross product, NOTE: not all files were converted to mpm and not all angle calculations were changed to cross product

correction_of_track.py
@@ -1,12 +1,13 @@
 import os
 import matplotlib.pyplot as plt
-import numpy as np
+#import numpy as np
+import mpmath as mpm
 #from trajectory_planning_helpers.calc_normal_vectors import calc_normal_vectors
 from racetrack_library import metodo2, load_track_points,load_racing_line_points, plot_tracks, correttore, curva_direzione,intersect
 
 def correction_of_track(points):
     w_right_x, w_right_y, w_left_x, w_left_y, center_x, center_y, normals = metodo2(points)
-    thetas=np.zeros(len(points))
+    thetas=mpm.matrix([0]*len(points))
 
     #controllo se le normali si intreccianos
     p=[]
@@ -30,25 +31,25 @@ def correction_of_track(points):
         if(direzione==-1):
             w_left_x_mod, w_left_y_mod, w_right_x_mod, w_right_y_mod, t, nn = correttore(problems, w_left_x, w_left_y, points, w_right_x, w_right_y)
             for i in range(len(problems)):
-                if t==np.nan:
+                if mpm.isnan(t):
                     print(filename)
                 thetas[problems[i]]=t[i]
-                new_points[problems[i]][3]=np.sqrt(np.power(w_left_x_mod[problems[i]]-points[problems[i]][0],2) + np.power(w_left_y_mod[problems[i]]-points[problems[i]][1],2)) 
+                new_points[problems[i],3]=mpm.sqrt(mpm.power(w_left_x_mod[problems[i]]-points[problems[i],0],2) + mpm.power(w_left_y_mod[problems[i]]-points[problems[i],1],2)) 
                 x,y=nn[i]
                 new_normals[problems[i]]=[-x,-y]
         elif(direzione==1):
             w_right_x_mod, w_right_y_mod, w_left_x_mod, w_left_y_mod, t, nn = correttore(problems, w_right_x, w_right_y, points, w_left_x, w_left_y)
             for i in range(len(problems)):
-                if t==np.nan:
+                if mpm.isnan(t):
                     print(filename)
                 thetas[problems[i]]=t[i]
-                new_points[problems[i]][2]=np.sqrt(np.power(w_left_x_mod[problems[i]]-points[problems[i]][0],2) + np.power(w_left_y_mod[problems[i]]-points[problems[i]][1],2)) 
+                new_points[problems[i],2]=mpm.sqrt(mpm.power(w_left_x_mod[problems[i]]-points[problems[i],0],2) + mpm.power(w_left_y_mod[problems[i]]-points[problems[i],1],2)) 
                 x,y=nn[i]
                 new_normals[problems[i]]=[x,y]
                 #TODO decide if also save the new normals
     
         return  w_left_x_mod, w_left_y_mod, new_points, w_right_x_mod, w_right_y_mod, thetas, new_normals
-    return      w_left_x, w_left_y, points, w_right_x, w_right_y,np.zeros(len(w_left_x)), normals
+    return      w_left_x, w_left_y, points, w_right_x, w_right_y,thetas, normals
 
 if __name__ == "__main__":
 
@@ -63,14 +64,14 @@ if __name__ == "__main__":
     track_path = os.path.join(tracks_dir, filename)
     racing_line_path= os.path.join(racing_line_dir, filename)
 
-    points = np.array(load_track_points(track_path))
-    raceline=np.array(load_racing_line_points(racing_line_path))
+    points = mpm.matrix(load_track_points(track_path))
+    raceline=mpm.matrix(load_racing_line_points(racing_line_path))
 
     fig, axs = plt.subplots(1, 2, figsize=(6,12))
     w_right_x, w_right_y, w_left_x, w_left_y, center_x, center_y, normals = metodo2(points)
     w_left_x_mod, w_left_y_mod, new_points, w_right_x_mod, w_right_y_mod, thetas, new_normals=correction_of_track(points)    
 
-    set_diff = np.setdiff1d(new_normals, normals)
+    #set_diff = np.setdiff1d(new_normals, normals)
     #print(set_diff)
 
     plot_tracks(axs[0],w_left_x,w_left_y,points,w_right_x,w_right_y,raceline,filename)

load_data_as_tensor_v2.py
@@ -3,15 +3,24 @@
 from racetrack_feature_extraction_v2 import racetrack_feature_extraction
 import numpy as np
 import torch
+import time
 
-def load_data_as_tensor(tracks_dir,racing_line_dir,filename,with_thetas,total_foresight,sampling):
-    l,alpha,thetas,dists,raceline=racetrack_feature_extraction(tracks_dir,racing_line_dir,filename)
+def load_data_as_tensor(tracks_dir,racing_line_dir,filename,with_thetas,with_dists,total_foresight,sampling):
     
-    # depends if we know wehter there are modified normals in the tracks
-    if with_thetas==1:
-        features = np.array((l, alpha,dists,thetas))
+    res=racetrack_feature_extraction(tracks_dir,racing_line_dir,filename,with_dists)
+    if(with_dists!=0):
+        l,alpha,thetas,dists,raceline=res
     else:
+        l,alpha,thetas,raceline=res
+    # depends if we know wehter there are modified normals in the tracks
+    if with_dists==1 and with_thetas==1:
+        features = np.array((l, alpha,dists,thetas))
+    elif with_dists==1 and with_thetas==0:
         features = np.array((l,alpha,dists))
+    elif with_dists==0 and with_thetas==1:
+        features = np.array((l, alpha,thetas))
+    else: 
+        features = np.array((l, alpha))
     #print(features.shape)
 
     track_length=len(l)
@@ -38,6 +47,19 @@ def load_data_as_tensor(tracks_dir,racing_line_dir,filename,with_thetas,total_fo
     # costruisci i target
     Y = np.take(raceline, output_idx)    # shape (track_length, output_size)
     # converti in tensori
-    X = torch.tensor(X, dtype=torch.float32)
-    Y = torch.tensor(Y, dtype=torch.float32)
+
+    X = torch.tensor(X, dtype=torch.float64)
+    Y = torch.tensor(Y, dtype=torch.float64)
+    
     return X,Y
+
+if __name__== "__main__":
+    t=time.time()
+    tracks_dir = "tracks/train/tracks"
+    racing_line_dir = "tracks/train/racelinesCorrected"
+    filename="Shanghai.csv"    
+    
+    X,Y =load_data_as_tensor(tracks_dir,racing_line_dir,filename,0,1,70,4)
+
+    print(X.shape,Y.shape)
+    print(time.time()-t)

raceline_prediction_model_v2.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 import os
 import numpy as np
-from load_data_as_tensor import load_data_as_tensor
+
 from load_data_as_tensor_v2 import load_data_as_tensor as load_data_as_tensor_v2
 import time
 total_foresight=70 #basically f=35
@@ -25,19 +25,19 @@ class trackNet(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.model_stack=nn.Sequential(
-        nn.Linear(input_size,hidden_size1),
+        nn.Linear(input_size,hidden_size1,dtype=torch.float64),
         nn.Sigmoid(),
-        nn.Linear(hidden_size1,hidden_size2and3),
+        nn.Linear(hidden_size1,hidden_size2and3,dtype=torch.float64),
         nn.Sigmoid(),
-        nn.Linear(hidden_size2and3,hidden_size2and3),
+        nn.Linear(hidden_size2and3,hidden_size2and3,dtype=torch.float64),
         nn.Sigmoid(),
-        nn.Linear(hidden_size2and3,output_size),
+        nn.Linear(hidden_size2and3,output_size,dtype=torch.float64),
         nn.Hardsigmoid()
         )   
 
     def forward(self, x):
         # x = self.flatten(x)
-        logits = logits = self.model_stack(x)
+        logits = self.model_stack(x)
         return logits
 
 def train(filenames, model, loss_fn, optimizer):
@@ -48,10 +48,7 @@ def train(filenames, model, loss_fn, optimizer):
     
     for batch, filename in enumerate(filenames):
         #print(batch)
-        if with_normal_lenght==0:
-            X,Y = load_data_as_tensor(tracks_dir,racing_line_dir,filename,with_thetas,total_foresight,sampling)
-        else:
-            X,Y = load_data_as_tensor_v2(tracks_dir,racing_line_dir,filename,with_thetas,total_foresight,sampling)
+        X,Y = load_data_as_tensor_v2(tracks_dir,racing_line_dir,filename,with_thetas,with_normal_lenght,total_foresight,sampling)
         track_length=X.shape[0]
         #print(X.shape, Y.shape)
         
@@ -79,10 +76,7 @@ def test(filenames, model, loss_fn):
     with torch.no_grad():
         for filename in filenames:
             # carica i dati e li porta sul device corretto
-            if with_normal_lenght==0:
-                X,Y = load_data_as_tensor(tracks_dir,racing_line_dir,filename,with_thetas,total_foresight,sampling)
-            else:
-                X,Y = load_data_as_tensor_v2(tracks_dir,racing_line_dir,filename,with_thetas,total_foresight,sampling)
+            X,Y = load_data_as_tensor_v2(tracks_dir,racing_line_dir,filename,with_thetas,with_normal_lenght,total_foresight,sampling)
             X, Y = X.to(device), Y.to(device)
 
             # forward
@@ -102,12 +96,12 @@ def test(filenames, model, loss_fn):
     return avg_loss,avg_r2,avg_poiss
 
 clock=time.time()
-tracks_dir = "tracks/train/tracks"
+tracks_dir = "tracks/backup/tracks"
 racing_line_dir = "tracks/train/racelinesCorrected"
 #uso il singolo circuito come batch? si dai
 filenames =np.array( [f for f in os.listdir(tracks_dir) if os.path.isfile(os.path.join(tracks_dir, f))])
-usable_data, valuation_data = train_test_split(filenames,test_size=0.1)
-train_data,test_data=train_test_split(usable_data,test_size=0.2)
+#usable_data, valuation_data = train_test_split(filenames,test_size=0.1)
+train_data,test_data=train_test_split(filenames,test_size=0.2)
 
 print(time.time()-clock)
 clock=time.time()
@@ -131,7 +125,7 @@ for t in range(epochs):
     train(train_data,net,loss_fn,optimizer)
     
     avg_loss,r2,poiss=test(test_data,net,loss_fn)
-    print(f"Test Error: Avg loss: {avg_loss:>8f}, r2: {r2:>8f}, mean poisson deviance: {poiss:>8f}")
+    print(f"Test Error:        Avg loss: {avg_loss:>8f}, r2: {r2:>8f}, mean poisson deviance: {poiss:>8f}")
     if t!=0:
         print(f"miglioramento del: Avg loss: {(prec-avg_loss)/prec*100:>8f}% r2: {(prec_r2-r2)/prec_r2*100:>8f}% mean poisson deviance: {(prec_poiss-poiss)/1*100:>8f}%")
     prec=avg_loss

racetrack_feature_extraction_v2.py
@@ -2,55 +2,79 @@ from correction_of_track import correction_of_track
 from racetrack_library import load_racing_line_modified,load_track_points
 import numpy as np
 from os.path import join
-
-
-def racetrack_feature_extraction(tracks_dir,racing_line_dir,filename):
+import mpmath as mpm
+                                                                    #difference between v1 and v2
+def racetrack_feature_extraction(tracks_dir,racing_line_dir,filename,with_distances):
 
     #ottieni i punti dal file
     track_path = join(tracks_dir, filename)
     racing_line_path = join(racing_line_dir, filename)
-    points =   np.array(load_track_points(track_path))
+    points =   mpm.matrix(load_track_points(track_path))
     
     #raceline  è gia rappresentato come [0,1] ovvero una posizione relativa sulla normale rispettiva
-    raceline = np.array(load_racing_line_modified(racing_line_path))
+    raceline = mpm.matrix(load_racing_line_modified(racing_line_path))
 
     #"corregge" i punti (in realtà se il tracciato è a posto non fa nulla e thetas=np.zeros)
     left_x, left_y, points, right_x, right_y, thetas, normals=correction_of_track(points)
-
-    #ottiene la lunghezza delle normali
-    l=np.sqrt(np.power(right_x-left_x,2) + np.power(right_y-left_y,2)) 
-
-    #normal= np.array([-dy, dx])
-    derivates=np.zeros(normals.shape)
     
-    derivates[:,[0,1]]= normals[:,[1,0]]
+    #ottiene la lunghezza delle normali
+    l=mpm.matrix(left_x.rows,1)
+    for i in range(len(right_x)):
+        l[i]=mpm.sqrt((right_x[i]-left_x[i])**2+(right_y[i]-left_y[i])**2)
+
+    #normal= np.array([-dy, dx])RE
+
+    derivates=mpm.zeros(normals.rows, 2)
+    
+    derivates[:,0]= normals[:,1]
+    derivates[:,1]= normals[:,0]
     derivates[:,1]=-derivates[:,1]
     
-    distances=[]
+    len_derivates=len(normals)
+    len_points=len(points)
+    distances=mpm.matrix(len_derivates,1)
     #ottiene l'angolo tra 2 normali consecutive (in teoria TODO controllare)
     #!!! radians !!!
-    alpha=[]
-    for i in range(len(derivates)-1):
-        dot=np.dot(derivates[i],derivates[i+1])
-        alpha.append(2*np.pi-np.arccos(np.clip(dot, -1.0, 1.0)))
+    alpha=mpm.matrix(len_derivates,1)
+    for i in range(len_derivates-1):
+        # dot=mpm.fdot(derivates[i,:],derivates[i+1,:])
+        # if(dot<=0):
+        #     print(dot)
+        # alpha[i]=mpm.acos(dot)
+        cross = derivates[i,1]*derivates[i+1,0] - derivates[i,0]*derivates[i+1,1]
+        alpha[i]=mpm.asin(cross)
 
-        distances.append(np.sqrt((points[i+1][0]-points[i][0])**2+(points[i+1][1]-points[i][1])**2))
+        distances[i]=mpm.sqrt((points[i+1,0]-points[i,0])**2+(points[i+1,1]-points[i,1])**2)
 
-    dot=np.dot(derivates[-1],derivates[0])
-    alpha.append(2*np.pi-np.arccos(np.clip(dot, -1.0, 1.0)))
+    # dot=mpm.fdot(normals[0,:],normals[len_derivates-1,:])
+    # alpha[len(normals)-1]=mpm.acos(dot)
+    cross = derivates[len_derivates-1,1]*derivates[0,0] - derivates[len_derivates-1,0]*derivates[0,1]
+    alpha[len_derivates-1]=cross
+    distances[len(normals)-1]=mpm.sqrt((points[0,0]-points[len_points-1,0])**2+(points[0,1]-points[len_points-1,1])**2)
+
+    s=mpm.mpf("0")
+    for i in range(len(alpha)):
+        s+=alpha[i]
     
-    distances.append(np.sqrt((points[0][0]-points[-1][0])**2+(points[0][1]-points[-1][1])**2))
 
-    return l,alpha,thetas,distances,raceline
+    #riconverto in np perchè piu semplice poi passare a tensor
+    l=np.array(l.tolist(), dtype=np.float64)
+    alpha=np.array(alpha.tolist(), dtype=np.float64)
+    thetas=np.array(thetas.tolist(), dtype=np.float64)
+    distances=np.array(distances.tolist(), dtype=np.float64)
+    raceline=np.array(raceline.tolist(), dtype=np.float64)
 
+    if with_distances!=0:
+        return l,alpha,thetas,distances,raceline
+    return l,alpha,thetas,raceline
 
 #TEST
 if __name__ == "__main__":
     tracks_dir = "tracks/train/tracks"
     racing_line_dir = "tracks/train/racelinesCorrected"
-    filename="NorisringFlippedX.csv"    
+    filename="Shanghai.csv"    
     
-    l,alpha,thetas,raceline=racetrack_feature_extraction(tracks_dir,racing_line_dir,filename)
+    l,alpha,thetas,distances,raceline=racetrack_feature_extraction(tracks_dir,racing_line_dir,filename,1)
 
     print(len(l))
     print(len(alpha))

racetrack_library.py
@@ -1,7 +1,7 @@
 import os
 import matplotlib.pyplot as plt
-import numpy as np
-
+#import numpy as np
+import mpmath as mpm
 def load_track_points(track_file):
     with open(track_file, "r") as f:
         lines = f.readlines()
@@ -9,7 +9,7 @@ def load_track_points(track_file):
     for line in lines:
         parts = line.strip().split(",")
         if len(parts) == 4 and "#"!=line[0]:
-            x, y, w_right, w_left = map(float, parts)
+            x, y, w_right, w_left = [mpm.mpf(p) for p in parts]
             points.append((x, y, w_right, w_left))
     return points
 
@@ -20,7 +20,7 @@ def load_racing_line_points(racing_line_file):
     for line in lines:
         parts = line.strip().split(",")
         if len(parts) == 2 and "#"!=line[0]:
-            x, y = map(float, parts)
+            x, y = [mpm.mpf(p) for p in parts]
             points.append((x, y))
     return points
 
@@ -31,7 +31,7 @@ def load_racing_line_modified(racing_line_file):
     for line in lines:
         parts = line.strip().split(",")
         if len(parts) == 1 and "#"!=line[0]:
-            w = float(parts[0])
+            w = mpm.mpf(parts[0])
             points.append(w)
     return points
 
@@ -46,42 +46,37 @@ def derivate(prec,p,succ):
     return dx,dy
 
 def metodo2(points):
-    w_right_x=[]
-    w_right_y=[]
-    w_left_x=[]
-    w_left_y=[]
-    center_x=[]
-    center_y=[]
-    normals=[]
+    tot_length=len(points)
+    w_right_x=mpm.matrix(tot_length,1)
+    w_right_y=mpm.matrix(tot_length,1)
+    w_left_x=mpm.matrix(tot_length,1)
+    w_left_y=mpm.matrix(tot_length,1)
+    center_x=mpm.matrix(tot_length,1)
+    center_y=mpm.matrix(tot_length,1)
+    normals=mpm.matrix(tot_length,2)
     # Calcola le direzioni tra i punti per disegnare le larghezze
     for i in range(len(points)):
         if i == 0:
-            dx,dy=derivate(points[-1],points[0],points[1])
+            dx,dy=derivate(points[len(points)-1,:],points[0,:],points[1,:])
         elif i == len(points) - 1:
-            dx,dy=derivate(points[i-1],points[i],points[0])
+            dx,dy=derivate(points[i-1,:],points[i,:],points[0,:])
         else:
-            dx,dy=derivate(points[i-1],points[i],points[i+1])
+            dx,dy=derivate(points[i-1,:],points[i,:],points[i+1,:])
 
     # Vettore perpendicolare normalizzato
-        perp = np.array([-dy, dx])
-        perp /= np.linalg.norm(perp)
-        normals.append(perp)
+        perp = mpm.matrix([-dy, dx])
+        perp /= mpm.norm(perp)
+        normals[i,:]=perp.T
     # Calcolo bordo destro e sinistro
-        cx, cy = points[i][0], points[i][1]
-        wR, wL = points[i][2], points[i][3]
-        w_right_x.append(cx - perp[0] * wR)
-        w_right_y.append(cy - perp[1] * wR)
-        w_left_x.append(cx + perp[0] * wL)
-        w_left_y.append(cy + perp[1] * wL)
-        center_x.append((w_right_x[-1] + w_left_x[-1]) / 2)
-        center_y.append((w_right_y[-1] + w_left_y[-1]) / 2)
+        cx, cy = points[i,0], points[i,1]
+        wR, wL = points[i,2], points[i,3]
+        w_right_x[i]=cx - perp[0] * wR
+        w_right_y[i]=cy - perp[1] * wR
+        w_left_x[i]=cx + perp[0] * wL
+        w_left_y[i]=cy + perp[1] * wL
+        center_x[i]=(w_right_x[i] + w_left_x[i]) / 2
+        center_y[i]=(w_right_y[i] + w_left_y[i]) / 2
 
-    normals=np.array(normals)
-    w_right_x=np.array(w_right_x)
-    w_right_y=np.array(w_right_y)
-    w_left_x=np.array(w_left_x)
-    center_x=np.array(center_x)
-    center_y=np.array(center_y)
     return w_right_x, w_right_y, w_left_x, w_left_y, center_x, center_y, normals
 
 def plot_tracks_already_calced_normals(points, normals):
@@ -93,9 +88,9 @@ def plot_tracks_already_calced_normals(points, normals):
     center_x=[]
     center_y=[]
     for i in range(len(points)):
-        perp = np.array([normals[i][0],normals[i][1]])
-        cx, cy = points[i][0], points[i][1]
-        wR, wL = points[i][2], points[i][3]
+        perp = mpm.matrix([normals[i,0],normals[i,1]])
+        cx, cy = points[i,0], points[i,1]
+        wR, wL = points[i,2], points[i,3]
         w_right_x.append(cx - perp[0] * wR)
         w_right_y.append(cy - perp[1] * wR)
         w_left_x.append(cx + perp[0] * wL)
@@ -113,22 +108,23 @@ def plot_tracks(axs,left_x,left_y,centerline,right_x,right_y,raceline, filename)
         xr, yr = right_x[i], right_y[i]
         xl, yl = left_x[i], left_y[i]
         axs.plot([xr, xl], [yr, yl], 'g--', linewidth=0.5)
+        axs.plot([centerline[i,0], xl], [centerline[i,1], yl], 'r--', linewidth=0.5)
 
 def curva_direzione(prev_point, curr_point, next_point):
     # Vettore dal punto precedente al corrente
-    v1 = np.array([curr_point[0] - prev_point[0], curr_point[1] - prev_point[1]])
+    v1 = mpm.matrix([curr_point[0] - prev_point[0], curr_point[1] - prev_point[1]])
     # Vettore dal corrente al successivo
-    v2 = np.array([next_point[0] - curr_point[0], next_point[1] - curr_point[1]])
+    v2 = mpm.matrix([next_point[0] - curr_point[0], next_point[1] - curr_point[1]])
     # Calcola il prodotto vettoriale (solo la componente z)
     cross = v1[1]*v2[0] - v1[0]*v2[1]
     # Calcola il prodotto scalare per l'angolo
-    dot = np.dot(v1, v2)
-    norm_v1 = np.linalg.norm(v1)
-    norm_v2 = np.linalg.norm(v2)
+    dot = mpm.fdot(v1, v2)
+    norm_v1 = mpm.norm(v1)
+    norm_v2 = mpm.norm(v2)
     if norm_v1 == 0 or norm_v2 == 0:
         return 0  # Evita divisione per zero
-    angle = np.arccos(np.clip(dot / (norm_v1 * norm_v2), -1.0, 1.0))
-    leeway = np.deg2rad(5)  # ad esempio 5 gradi di tolleranza
+    angle = mpm.acos(dot / (norm_v1 * norm_v2))
+    leeway = mpm.mpf('0.03')  # ad esempio 5 gradi di tolleranza
     if abs(angle) < leeway:
         return 0  # rettilineo
     if cross > 0:
@@ -146,10 +142,10 @@ def correttore(problems, problem_side_x, problem_side_y, centerline, opposite_si
     opposite_side_x_modified=opposite_side_x.copy()
     opposite_side_y_modified=opposite_side_y.copy()
     for i in problems_modified:
-        closest=np.inf
+        closest=mpm.inf
         closest_indx=None
         for j in problems_modified:
-            t=np.sqrt(np.power(problem_side_x_modified[j]-problem_side_x_modified[i-1],2) + np.power(problem_side_y_modified[j]-problem_side_y_modified[i-1],2))
+            t=mpm.sqrt(mpm.power(problem_side_x_modified[j]-problem_side_x_modified[i-1],2) + mpm.power(problem_side_y_modified[j]-problem_side_y_modified[i-1],2))
             if(t<closest):
                 closest=t
                 closest_indx=j
@@ -165,30 +161,30 @@ def correttore(problems, problem_side_x, problem_side_y, centerline, opposite_si
         problems_modified.remove(i)
     
     #calcolo di quanto è stato modificato l'angolo
-    thetas=[]
-    new_normals=[]
+    thetas=mpm.matrix(len(problems),1)
+    new_normals=mpm.matrix(len(problems),2)
     for j in problems:
                         #nuovo x               #centro         #vecchio x        #centro             #nuovo y              #centro              #vecchio y        #centro
-        Ax = problem_side_x[j] - centerline[j][0]
-        Ay = problem_side_y[j] - centerline[j][1]
-        Bx = problem_side_x_modified[j] - centerline[j][0]
-        By = problem_side_y_modified[j] - centerline[j][1]
+        Ax = problem_side_x[j] - centerline[j,0]
+        Ay = problem_side_y[j] - centerline[j,1]
+        Bx = problem_side_x_modified[j] - centerline[j,0]
+        By = problem_side_y_modified[j] - centerline[j,1]
 
-        dotprod = np.dot([Ax,Ay],[Bx,By])
-        lengtha = np.sqrt(Ax**2 + Ay**2)
-        lengthb = np.sqrt(Bx**2 + By**2)
+        dotprod = mpm.dot([Ax,Ay],[Bx,By])
+        lengtha = mpm.sqrt(Ax**2 + Ay**2)
+        lengthb = mpm.sqrt(Bx**2 + By**2)
 
         #!!! radians !!!
-        theta = np.arccos(dotprod / (lengtha * lengthb))
-        thetas.append(theta)
+        theta = mpm.acos(dotprod / (lengtha * lengthb))
+        thetas[j]=theta
             #normale nuova
-        n=[(problem_side_x_modified[j]-centerline[j][0])/lengthb, (problem_side_y_modified[j]-centerline[j][1])/lengthb]
-        new_normals.append(n)
+        n=[(problem_side_x_modified[j]-centerline[j,0])/lengthb, (problem_side_y_modified[j]-centerline[j,1])/lengthb]
+        new_normals[j,:]=n
        
         
-        lunghezza_opposta=np.sqrt(np.power(opposite_side_x[j]-centerline[j][0],2) + np.power(opposite_side_y[j]-centerline[j][1],2)) 
-        opposite_side_x_modified[j]=centerline[j][0]-n[0]*lunghezza_opposta
-        opposite_side_y_modified[j]=centerline[j][1]-n[1]*lunghezza_opposta
+        lunghezza_opposta=mpm.sqrt(mpm.power(opposite_side_x[j]-centerline[j,0],2) + mpm.power(opposite_side_y[j]-centerline[j,1],2)) 
+        opposite_side_x_modified[j]=centerline[j,0]-n[0]*lunghezza_opposta
+        opposite_side_y_modified[j]=centerline[j,1]-n[1]*lunghezza_opposta
 
     return problem_side_x_modified, problem_side_y_modified, opposite_side_x_modified, opposite_side_y_modified, thetas, new_normals
 
@@ -206,9 +202,9 @@ def calc_intersect(A,B,C,D):
     xt=A[0]+t*(B[0]-A[0])
     yt=A[1]+t*(B[1]-A[1])
     
-    numu=(A[0]-B[0])*(A[1]-C[1])-(A[1]-B[1])*(A[0]-C[0])
-    denu=(A[0]-B[0])*(C[1]-D[1])-(A[1]-B[1])*(C[0]-D[0])
-    u=numu/denu
+    # numu=(A[0]-B[0])*(A[1]-C[1])-(A[1]-B[1])*(A[0]-C[0])
+    # denu=(A[0]-B[0])*(C[1]-D[1])-(A[1]-B[1])*(C[0]-D[0])
+    # u=numu/denu
     
     return xt,yt,t
 