ff8b25b65a93d4c99be198a3c58db273e503e2eb
Stefano Cattonar <stefano.cattonar@outlook.com>
2025-08-23 15:18:46 +0200

    Add resume

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+#model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+
+model = YOLO("runs/detect/train/weights/last.pt")
 
 # Train the model
-results = model.train(data="fsoco_test.yaml", epochs=100, imgsz=640)
+#results = model.train(resume=True, epochs=15, imgsz=640, device="cpu")
+model.train(resume=False, epochs=1, imgsz=640, device="cpu")
