5416d902bf68d999163e05c06bb247558f66d203
alexsockz <valessiobg@gmail.com>
2025-08-27 13:10:31 +0200

    corrected a result

--- raceline_prediction_model_v2.py
+++ raceline_prediction_model_v2.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torchmetrics import TweedieDevianceScore, R2Score
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split
 import copy
 import os
 import numpy as np
