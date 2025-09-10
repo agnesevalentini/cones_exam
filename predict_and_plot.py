6d149bd463e1e3f94e90ddfd5398d096cd68cd9c
alexsockz <valessiobg@gmail.com>
2025-09-10 14:34:21 +0200

    finished merges and created predict_and_plot.py

---
+++ predict_and_plot.py
@@ -0,0 +1,4 @@
+#plotta i predict
+#ricalcola il modello per 3 
+#modifica 
+#modifica un po il predict in modo che riceva solo la posizione iniziale e riutilizzi le sue predizioni per calcolare gruppo di dati successivo
